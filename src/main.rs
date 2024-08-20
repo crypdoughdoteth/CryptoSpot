fe05cb5c7c079f7cef89f19caa6ad9ae4740fcf6
crypdoughdoteth <zkorn1@icloud.com>
2024-08-19 23:58:56 -0400

    "Add 24 hr % change & color code table"

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,26 @@
 use anyhow::{bail, Result};
 use dialoguer::{theme::ColorfulTheme, FuzzySelect, MultiSelect};
 use serde::{Deserialize, Serialize};
+use tabled::settings::object::Rows;
+use tabled::settings::{themes::Colorization, Color};
 use tabled::{settings::Style, Table, Tabled};
 
-#[derive(Serialize, Deserialize, Debug)]
-struct PriceData {
-    data: AssetData,
-}
-
-#[derive(Serialize, Deserialize, Debug, Tabled)]
-struct AssetData {
-    amount: String,
+#[derive(Debug, Clone, Tabled)]
+pub struct CoinbasePriceData {
+    current_price: f64,
+    daily_percent: f64,
     base: String,
     currency: String,
 }
 
 const ASSETS: &[&str] = &[
-    "ETH", "BTC", "SUI", "NAVX", "CETUS", "SOL", "SEI", "TIA", "APT", "MATIC", "FTM", "OP", "ARB", "LINK", "DOT",
-    "ADA", "AVAX", "LUNA", "ATOM", "ALGO", "XLM", "XRP", "DOGE", "SHIB", "LTC", "BCH", "EOS", "XTZ",
+    "ETH", "BTC", "SUI", "SOL", "DOT", "SEI", "TIA", "APT", "MATIC", "FTM", "OP", "ARB", "LINK",
+    "ADA", "HBAR", "AVAX", "ATOM", "ALGO", "XLM", "XRP", "DOGE", "SHIB", "LTC", "BCH", "EOS",
+    "XTZ",
 ];
-const BASE: &[&str] = &["USD", "CAD", "AUD", "INR", "EUR", "GBP", "COP", "JPY", "CNY", "HKD"];
+const BASE: &[&str] = &[
+    "USD", "CAD", "AUD", "INR", "EUR", "GBP", "COP", "JPY", "CNY", "HKD",
+];
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -34,38 +35,141 @@ async fn main() -> Result<()> {
         .items(BASE)
         .default(0)
         .interact()?];
-    
+
     if selection.is_empty() {
         bail!("No assets selected");
     }
 
-    let mut handles = Vec::new();
-
+    let selection_size = selection.len();
+    let mut handles = Vec::with_capacity(selection_size);
     for i in selection.into_iter() {
-        let handle: tokio::task::JoinHandle<anyhow::Result<PriceData>> = tokio::spawn(async move {
-            let asset = ASSETS[i];
-            let url = format!(
-                "https://api.coinbase.com/v2/prices/{}-{}/spot",
-                asset, base_selection
-            );
-            let price: PriceData = reqwest::Client::new()
-                .get(url)
-                .send()
-                .await?
-                .json::<PriceData>()
-                .await?;
-            Ok(price)
-        });
+        let handle: tokio::task::JoinHandle<anyhow::Result<(CoinbasePriceData, Color)>> =
+            tokio::spawn(async move {
+                let asset = ASSETS[i];
+                let url = format!(
+                    "https://api.coinbase.com/v2/prices/{}-{}/historic?period=day",
+                    asset, base_selection
+                );
+
+                let prices: HistoricalPriceData = reqwest::Client::new()
+                    .get(url)
+                    .send()
+                    .await?
+                    .json::<HistoricalPriceData>()
+                    .await?;
+
+                let recent = prices
+                    .data
+                    .prices
+                    .first()
+                    .unwrap()
+                    .price
+                    .parse::<f64>()
+                    .unwrap();
+
+                let day_ago = prices
+                    .data
+                    .prices
+                    .last()
+                    .unwrap()
+                    .price
+                    .parse::<f64>()
+                    .unwrap();
+
+                let percent_diff = ((recent - day_ago).abs() / ((recent + day_ago) / 2.0)) * 100.0;
+
+                let res = CoinbasePriceData {
+                    current_price: recent,
+                    daily_percent: percent_diff,
+                    base: base_selection.to_string(),
+                    currency: asset.to_string(),
+                };
+
+                let daily: Color = match (recent, day_ago) {
+                    (r, d) if r > d => Color::BG_GREEN,
+                    (r, d) if r < d => Color::BG_RED,
+                    _ => Color::BG_WHITE,
+                };
+
+                Ok((res, daily))
+            });
         handles.push(handle);
     }
 
-    let mut results = vec![];
+    let mut results = Vec::with_capacity(selection_size);
+    let mut colors = Vec::with_capacity(selection_size);
     for handle in handles {
-        results.push(handle.await??.data);
+        let res = handle.await??;
+        results.push(res.0);
+        colors.push(res.1);
     }
 
-    let table = Table::new(results).with(Style::modern()).to_string();
+    let table = Table::new(results)
+        .with(Style::modern())
+        .with(Colorization::exact(colors, Rows::new(1..)))
+        .to_string();
+
     println!("\n{}", table);
 
     Ok(())
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct HistoricalPriceData {
+    data: HistoricalAsset,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct HistoricalAsset {
+    base: String,
+    currency: String,
+    prices: Vec<HistoricalPrice>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct HistoricalPrice {
+    price: String,
+    time: String,
+}
+
+#[cfg(test)]
+pub mod test {
+
+    use super::*;
+
+    #[tokio::test]
+    pub async fn midnight_price() -> Result<(), Box<dyn std::error::Error>> {
+        let url = "https://api.coinbase.com/v2/prices/ETH-USD/historic?period=day";
+        let prices: HistoricalPriceData = reqwest::Client::new()
+            .get(url)
+            .send()
+            .await?
+            .json::<HistoricalPriceData>()
+            .await?;
+
+        let recent = prices
+            .data
+            .prices
+            .first()
+            .unwrap()
+            .price
+            .parse::<f64>()
+            .unwrap();
+
+        let day_ago = prices
+            .data
+            .prices
+            .last()
+            .unwrap()
+            .price
+            .parse::<f64>()
+            .unwrap();
+
+        let percent_diff = ((recent - day_ago).abs() / ((recent + day_ago) / 2.0)) * 100.0;
+
+        println!("{:?}", (recent, day_ago));
+        println!("{:?}", percent_diff);
+
+        Ok(())
+    }
 }
